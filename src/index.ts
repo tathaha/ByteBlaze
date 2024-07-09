ed25d4fbf0b34025577917afa5c3199c0adab805
Chiraitori <77224364+tathaha@users.noreply.github.com>
2024-07-09 18:35:22 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,14 @@ configData.bot.TOKEN.forEach((token, index) => {
   byteblaze.start();
   byteblaze.login(token);
 });
+
+const app: Express = express();
+const port = 9000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
